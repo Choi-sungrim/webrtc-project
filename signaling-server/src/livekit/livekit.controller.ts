a09fedd7967011d3d80965f32236c8cf7570c367
Sungrim Choi <choisr@robotis.com>
2025-10-16 11:17:35 +0900

    FIX : MultiUser connection, livekit FR , Func Roomlists, Func join update

--- signaling-server/src/livekit/livekit.controller.ts
+++ signaling-server/src/livekit/livekit.controller.ts
@@ -1,21 +1,27 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { LivekitService } from './livekit.service';
-import { AccessToken } from 'livekit-server-sdk';
 
 @Controller('livekit')
 export class LivekitController {
   constructor(private readonly livekitService: LivekitService) {}
 
   @Get('token')
-  getLivekitToken(
-    @Query('room') room: string,
+  async getToken(
+    @Query('room') roomName: string,
     @Query('username') username: string,
-  ) {
-    if (!room || !username) {
-      return { error: 'Room and username are required.' };
-    }
-    const token = this.livekitService.getAccessToken(room, username);
-    console.log(token);
-    return { token };
+  ): Promise<{ token: string }> {
+    const jwtTokenString = await this.livekitService.getAccessToken(
+      roomName,
+      username,
+    );
+
+    return {
+      token: jwtTokenString,
+    };
+  }
+
+  @Get('rooms')
+  async listRooms() {
+    return await this.livekitService.listRooms();
   }
 }
